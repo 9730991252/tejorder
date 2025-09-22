3af9d615603dc8bda9121225f17694ba60964a03
9730991252 <sunilkale101@gmail.com>
2025-09-22 15:58:58 +0530

    ok

--- hotel/views.py
+++ hotel/views.py
@@ -56,16 +56,26 @@ def edit_pin(request):
 client = razorpay.Client(auth=(settings.RAZORPAY_KEY_ID, settings.RAZORPAY_KEY_SECRET))
     
 def check_payment(hotel):
-    hotel_payment = Hotel_Payment.objects.filter(hotel=hotel, is_paid=False, bills=None).last()
-    if hotel_payment:
-        if hotel_payment.razorpay_order_id:
+    try:
+        hotel_payment = Hotel_Payment.objects.filter(
+            hotel=hotel, is_paid=False, bills__isnull=True
+        ).last()
+
+        if hotel_payment and hotel_payment.razorpay_order_id:
             payments = client.order.payments(hotel_payment.razorpay_order_id)
-            if payments:
-                payment_paid = payments['items'][0]['status']
-                if payment_paid == 'captured':
-                    hotel_payment.is_paid = True
-                    hotel_payment.bills = hotel_payment.amount * 3
-                    hotel_payment.save()
+
+            if payments and 'items' in payments:
+                for p in payments['items']:
+                    if p.get('status') == 'captured':
+                        hotel_payment.is_paid = True
+                        hotel_payment.bills = hotel_payment.amount * 3
+                        hotel_payment.save()
+                        break
+
+    except Exception as e:
+        import traceback
+        print("Payment check error:", e)
+        traceback.print_exc()
 
 @csrf_exempt
 def software_charges(request):
